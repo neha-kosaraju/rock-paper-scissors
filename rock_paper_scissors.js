b01ca517935c2ae971d9ed2f0015d558347fc973
Neha <115666077+neha-kosaraju@users.noreply.github.com>
2023-02-21 15:49:43 -0500

    Edit rock_paper_scissors.js and index.html

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,28 +1,54 @@
-let options = ["Rock", "Paper", "Scissors"];
-let computer = options[Math.floor(Math.random()*options.length)]
-console.log(computer);
 
-function gameResults () {
-    if (player.toUpperCase() === computer.toUpperCase()) {
-        console.log("It's a tie!");
-    } else if (player.toUpperCase() === "ROCK" && computer.toUpperCase() === "PAPER") {
-        console.log("Better luck next time! Paper beats rock.");
-    } else if (player.toUpperCase() === "ROCK" && computer.toUpperCase() === "SCISSORS") {
-        console.log("Congrats, you win! Rock beats scissors.");
-    } else if (player.toUpperCase() === "PAPER" && computer.toUpperCase() === "SCISSORS") {
-        console.log("Better luck next time! Scissors beats paper.");
-    } else if (player.toUpperCase() === "PAPER" && computer.to.UpperCase() === "ROCK") {
-        console.log("Congrats, you win! Paper beats rock.");
-    } else if (player.toUpperCase() === "SCISSORS" && computer.toUpperCase() === "ROCK") {
-        console.log("Better luck next time! Rock beats scissors.");
-    } else if (player.toUpperCase() === "SCISSORS" && computer.toUpperCase() === "PAPER") {
-        console.log("Congrats, you win! Scissors beats paper.");
-    } else {
-        console.log("Please enter a valid choice: rock, paper or scissors.");
-    }
+let player = "";
+
+document.getElementById("rock").addEventListener("click", function(){
+    player = "Rock";
+    gameResults();
+});
+document.getElementById("paper").addEventListener("click", function() {
+    player = "Paper";
+    gameResults();
+});
+document.getElementById("scissors").addEventListener("click", function() {
+    player = "Scissors";
+    gameResults();
+});
+
+let gameNumber = document.getElementById("game").innerHTML;
+let computerPoints = document.getElementById("computer").innerHTML;
+let playerPoints = document.getElementsById("player").innerHTML;
+
+function gameResults() {
+
+    let options = ["Rock", "Paper", "Scissors"];
+    let computer = options[Math.round(Math.random()*options.length)];
+
+    gameNumber++;
     
+    if (player === "Rock" && computer === "Paper") {
+        document.getElementById("result-message").innerHTML = "Better luck next time! Paper beats rock.";
+        computerPoints++;
+    } else if (player === "Rock" && computer === "Scissors") {
+        document.getElementById("result-message").innerHTML = "Congrats, you win! Rock beats scissors.";
+        playerPoints++;
+    } else if (player === "Paper" && computer === "Scissors") {
+        document.getElementById("result-message").innerHTML = "Better luck next time! Scissors beats paper.";
+        computerPoints++;
+    } else if (player === "Paper" && computer === "Rock") {
+        document.getElementById("result-message").innerHTML = "Congrats, you win! Paper beats rock.";
+        playerPoints++;
+    } else if (player === "Scissors" && computer === "Rock") {
+        document.getElementById("result-message").innerHTML = "Better luck next time! Rock beats scissors.";
+        computerPoints++;
+    } else if (player === "Scissors" && computer === "Paper") {
+        document.getElementById("result-message").innerHTML = "Congrats, you win! Scissors beats paper.";
+        playerPoints++;
+    } else {
+        document.getElementById("result-message").innerHTML = "It's a tie!";
+        playerPoints++;
+        computerPoints++;
+    }
 
+    document.getElementById("game").innerHTML = gameNumber;
+    
 }
-
-let player = ;
-gameResults();
